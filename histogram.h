0f168505f9292805d9ffea59c3ff886dc4740fcf
Your Name <you@example.com>
2021-05-16 20:14:26 +0300

    code: то что не работало работает и сделано красиво

--- histogram.h
+++ histogram.h
@@ -4,6 +4,12 @@ using namespace std;
 #ifndef HISTOGRAM_H_INCLUDED
 #define HISTOGRAM_H_INCLUDED
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 void find_minmax(vector<double> numbers, double& min, double& max);
+vector<size_t> make_histogram(Input data);
+void show_histogram_text(const vector<size_t>& bins);
 
 #endif// HISTOGRAM_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -10,16 +10,6 @@
 
 using namespace std;
 
-struct Input{
-vector<double> numbers;
-size_t bin_count;
-};
-
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
-
-
-
 vector<double>
 input_numbers(istream& in,size_t count) {
     vector<double> result(count);
@@ -48,70 +38,11 @@ if(prompt)
 cerr <<"Enter bin count: ";
 
 size_t bin_count;
-in >> bin_count;
+in >> data.bin_count;
 
 return data;
 }
 
-vector<size_t>
-make_histogram(Input data) {
-    vector<size_t> bins(data.bin_count);
-    double min, max, bin_size;
-    find_minmax(data.numbers, min, max);
-    bin_size = (max - min) / data.bin_count;
-    for (double i : data.numbers)
-    {
-        bool found = false;
-        for (size_t j = 0; (j < data.bin_count - 1) && !found; j++)
-        {
-            auto lo = min + j * bin_size;
-            auto hi = min + (j + 1) * bin_size;
-            if ((lo <= i) && (i < hi))
-            {
-                bins[j]++;
-                found = true;
-            }
-        }
-        if (!found)
-        {
-            bins[data.bin_count - 1]++;
-        }
-    }
-    return bins;
-}
-
-void
-show_histogram_text(const vector<size_t>& bins) {
-    size_t max_count = bins[0];
-    for (size_t j : bins)
-    {
-        if (max_count < j)
-            max_count = j;
-    }
-
-    for (size_t j : bins)
-    {
-        if (j < 10)
-            cout << "  " << j << "|";
-        else  if (j < 100 && j>10)
-            cout << " " << j << "|";
-        else if (j >= 100)
-            cout << j << "|";
-        if (max_count == j)
-        {
-            for (size_t i = 0; i < MAX_ASTERISK; i++)
-                cout << "*";
-        }
-        else
-        {
-            size_t height = MAX_ASTERISK * (static_cast<double>(j) / max_count);
-            for (size_t q = 0; q < height; q++)
-                cout << "*";
-        }
-        cout << endl;
-    }
-    return;
-}
 
 size_t
 write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
@@ -134,12 +65,12 @@ download(const string& address) {
            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
            res = curl_easy_perform(curl);
-            if (res != CURLE_OK) {
-            cout << address<<endl;
-            cout << curl_easy_strerror(res);
-                exit(1);
+           if (res != CURLE_OK) {
+                  cout << address<<endl;
+                  cout << curl_easy_strerror(res);
+                  exit(1);
                }
-           curl_easy_cleanup(curl);
+    curl_easy_cleanup(curl);
            }
     return read_input(buffer, false);
 }
